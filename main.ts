306598fcf22e7a81fe551f050efa9aa3ca24cb35
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-09-01 13:18:22 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
+})
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("MOBILE.CSDPS", "2233avroyalequebec")
+basic.pause(5000)
 basic.forever(function () {
-	
+    ESP8266_IoT.connectThingSpeak()
+    ESP8266_IoT.setData(
+    "QDTG6PAVV5Q3297D",
+    Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_humidity) / 10,
+    Environment.ReadSoilHumidity(AnalogPin.P2),
+    Environment.ReadLightIntensity(AnalogPin.P1)
+    )
+    ESP8266_IoT.uploadData()
 })
