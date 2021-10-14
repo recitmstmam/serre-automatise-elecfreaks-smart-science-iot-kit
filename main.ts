791049b29935fba5a7c6286665389a3a164ec85d
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-10-14 08:28:37 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(1000)
     basic.showString("H air = " + Environment.octopus_BME280(Environment.BME280_state.BME280_humidity) + " %")
     basic.pause(1000)
-    basic.showString("Luminosité = " + Environment.ReadLightIntensity(AnalogPin.P3) + " %")
+    basic.showString("Luminosité = " + Environment.ReadLightIntensity(AnalogPin.P1) + " %")
     basic.pause(1000)
 })
 let range2: neopixel.Strip = null
@@ -18,45 +18,45 @@ basic.pause(1000)
 let Angle_fenêtre = 170
 let strip = neopixel.create(DigitalPin.P13, 40, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
-servos.P1.setAngle(Angle_fenêtre)
-servos.P2.setAngle(0)
+pins.servoWritePin(AnalogPin.P3, Angle_fenêtre)
+pins.servoWritePin(AnalogPin.P2, 0)
 basic.pause(2000)
-servos.P1.stop()
-servos.P2.stop()
+pins.digitalWritePin(DigitalPin.P3, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
 basic.forever(function () {
     if (Environment.ReadSoilHumidity(AnalogPin.P4) < 60) {
-        servos.P2.setAngle(160)
+        pins.servoWritePin(AnalogPin.P2, 160)
     } else if (Environment.ReadSoilHumidity(AnalogPin.P4) >= 60 && Environment.ReadSoilHumidity(AnalogPin.P4) <= 75) {
-        servos.P2.stop()
+        pins.digitalWritePin(DigitalPin.P2, 0)
     } else if (Environment.ReadSoilHumidity(AnalogPin.P4) > 75) {
-        servos.P2.setAngle(0)
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
     basic.pause(2000)
-    servos.P2.stop()
+    pins.digitalWritePin(DigitalPin.P2, 0)
     basic.pause(60000)
 })
 basic.forever(function () {
     if (Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C) >= 24) {
         while (Angle_fenêtre > 35) {
             Angle_fenêtre += -1
-            servos.P1.setAngle(Angle_fenêtre)
+            pins.servoWritePin(AnalogPin.P3, Angle_fenêtre)
             basic.pause(10)
         }
         basic.pause(1000)
-        servos.P1.stop()
+        pins.digitalWritePin(DigitalPin.P3, 0)
     } else {
         while (Angle_fenêtre < 170) {
             Angle_fenêtre += 1
-            servos.P1.setAngle(Angle_fenêtre)
+            pins.servoWritePin(AnalogPin.P3, Angle_fenêtre)
             basic.pause(10)
         }
         basic.pause(1000)
-        servos.P1.stop()
+        pins.digitalWritePin(DigitalPin.P3, 0)
     }
     basic.pause(60000)
 })
 basic.forever(function () {
-    if (Environment.ReadLightIntensity(AnalogPin.P10) <= 60) {
+    if (Environment.ReadLightIntensity(AnalogPin.P1) <= 40) {
         range = strip.range(0, 20)
         range2 = strip.range(20, 20)
         range.showColor(neopixel.colors(NeoPixelColors.White))
@@ -74,7 +74,7 @@ basic.forever(function () {
     Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
     Environment.octopus_BME280(Environment.BME280_state.BME280_pressure) / 10,
     Environment.ReadSoilHumidity(AnalogPin.P4),
-    Environment.ReadLightIntensity(AnalogPin.P10)
+    Environment.ReadLightIntensity(AnalogPin.P1)
     )
     ESP8266_IoT.uploadData()
 })
