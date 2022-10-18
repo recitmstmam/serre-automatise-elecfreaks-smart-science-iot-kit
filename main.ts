f1cc474be1232926031987d58b55968db479a397
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-18 10:47:02 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ input.onButtonPressed(Button.B, function () {
 let range2: neopixel.Strip = null
 let range: neopixel.Strip = null
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("MOBILE.CSDPS", "2233avroyalequebec")
+ESP8266_IoT.connectWifi("SOYEZenligne", "Abondance888")
 basic.pause(1000)
 let Angle_fenêtre = 170
 let strip = neopixel.create(DigitalPin.P13, 40, NeoPixelMode.RGB)
@@ -69,12 +69,12 @@ basic.forever(function () {
 basic.forever(function () {
     ESP8266_IoT.connectThingSpeak()
     ESP8266_IoT.setData(
-    "QDTG6PAVV5Q3297D",
+    "4XQAIH3DBRQBQ9Q9",
+    Environment.ReadSoilHumidity(AnalogPin.P4),
+    Environment.ReadLightIntensity(AnalogPin.P1),
     Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
     Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
-    Environment.octopus_BME280(Environment.BME280_state.BME280_pressure) / 10,
-    Environment.ReadSoilHumidity(AnalogPin.P4),
-    Environment.ReadLightIntensity(AnalogPin.P1)
+    Environment.octopus_BME280(Environment.BME280_state.BME280_pressure) / 10
     )
     ESP8266_IoT.uploadData()
 })
